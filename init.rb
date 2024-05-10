98a18b6007b23e19bf8b08115be2ee1193ff2f38
mingming <contact@mingming.wang>
2024-05-10 13:38:58 +0800

    refactor(info): change repo url

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_omniauth_cas_ucloud do
   description 'This plugin adds Omniauth support to Redmine'
   author 'mingming.wang'
   author_url 'mailto:contact@mingming.wang'
-  url 'https://github.com/mingming-cn'
+  url 'https://github.com/mingming-cn/redmine_omniauth_cas_ucloud'
   version '3.3.0'
   requires_redmine :version_or_higher => '2.0.0'
   settings :default => { 'onthefly_register' => 'false' },
